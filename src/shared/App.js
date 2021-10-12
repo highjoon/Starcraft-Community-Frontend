c962126c72618b095b383f6d68a78022affa1679
highjoon <yundosa2@gmail.com>
2021-10-12 10:45:03 +0900

    merge conflict resolve

--- src/shared/App.js
+++ src/shared/App.js
@@ -5,14 +5,18 @@ import { Route } from "react-router-dom";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
 
+import Header from "../components/Header";
 import Main from "../pages/Main";
+import PostWrite from "../pages/PostWrite";
 
 function App() {
     return (
         <React.Fragment>
             <GlobalStyle />
+            <Header />
             <ConnectedRouter history={history}>
                 <Route path="/" excat component={Main} />
+                <Route path="/write" excat component={PostWrite} />
             </ConnectedRouter>
         </React.Fragment>
     );
