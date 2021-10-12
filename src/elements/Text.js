079e83f78b22510e6230e071e1d2c21c3ed544c8
highjoon <yundosa2@gmail.com>
2021-10-13 01:29:10 +0900

    메인화면 종족 별 필터링

--- src/elements/Text.js
+++ src/elements/Text.js
@@ -2,9 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const Text = (props) => {
-    const { bold, color, size, children, margin } = props;
+    const { bold, color, size, children, margin, center } = props;
 
-    const styles = { bold: bold, color: color, size: size, margin };
+    const styles = { bold: bold, color: color, size: size, margin, center };
 
     return <P {...styles}>{children}</P>;
 };
@@ -15,13 +15,15 @@ Text.defaultProps = {
     color: "#161a1f",
     size: "20px",
     margin: false,
+    center: null,
 };
 
 const P = styled.p`
     color: ${(props) => props.color};
     font-size: ${(props) => props.size};
     font-weight: ${(props) => (props.bold ? "600" : "400")};
-    ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+    ${(props) => (props.margin ? `margin: ${props.margin};` : "")};
+    ${(props) => (props.center ? `text-align: center;` : "")};
 `;
 
 export default Text;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Category from "../components/Category";
 import Post from "../components/Post";
 import AddBtn from "../components/AddBtn";
 
 const Main = (props) => {
     const { history } = props;
+    const [categorize, setCategorize] = useState("전체보기");
     return (
         <React.Fragment>
-            <Category />
-            <Post />
+            <Category setCategorize={setCategorize} />
+            <Post categorize={categorize} />
             <AddBtn onClick={() => history.push("/write")} />
         </React.Fragment>
     );
