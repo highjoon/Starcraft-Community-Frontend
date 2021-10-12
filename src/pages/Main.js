d2a199dae1a202196357c27bf4160d9eb178fa9b
highjoon <yundosa2@gmail.com>
2021-10-12 22:42:52 +0900

    작성하기 버튼 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Category from "../components/Category";
 import Post from "../components/Post";
+import AddBtn from "../components/AddBtn";
 
 const Main = (props) => {
+    const { history } = props;
     return (
         <React.Fragment>
             <Category />
             <Post />
+            <AddBtn onClick={() => history.push("/write")} />
         </React.Fragment>
     );
 };
