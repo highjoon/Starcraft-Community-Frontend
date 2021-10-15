24e25609f84ef3020c278d99ba8f4f087b0f1ff8
dongyun <han10090109@gmail.com>
2021-10-15 18:05:06 +0900

    포스트 수정,삭제

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
-import { Text } from "../elements";
+import { Text, Button } from "../elements";
 import { actionCreators as postActions } from "../redux/modules/post";
+import { useHistory } from "react-router";
 
 const PostDetail = (props) => {
     const dispatch = useDispatch();
+    const history = useHistory();
     const postList = useSelector((state) => state.post.postList);
     const storedId = props.match.params.id;
 
@@ -26,6 +28,10 @@ const PostDetail = (props) => {
                             <TextContainer>
                                 <Text>{post.content}</Text>
                             </TextContainer>
+                            <BtnContainer>
+                                <Button margin="30px" height="40px" text="수정" _onClick={() => history.push(`/write/${post.id}`)} />
+                                <Button margin="30px" height="40px" text="삭제" _onClick={() => history.push("/")} />
+                            </BtnContainer>
                         </Wrapper>
                     );
                 }
@@ -60,6 +66,14 @@ const TextContainer = styled.div`
     background-color: #fff;
     box-shadow: 0 10px 10px rgba(0, 0, 0, 0.3);
     overflow-y: scroll;
+`;
+
+const BtnContainer = styled.div`
+    width: 500px;
+    margin: auto;
+    display: flex;
+    justify-content: space-around;
+
 `;
 
 export default PostDetail;
